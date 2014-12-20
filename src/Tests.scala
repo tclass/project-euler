d0533eb3d7b305bfb85371737a52797a62f5113b
tclass <t.klasse@googlemail.com>
2014-12-20 02:16:57 +0100

    changed main method

--- src/Tests.scala
+++ src/Tests.scala
@@ -1,14 +1,13 @@
 object Tests {
 
   def main(args: Array[String]) {
-    
-   val euler = new Euler();
-    
-   //val sum = euler.multiplies3And5(1000,0,0);
-   //val erg = euler.fibonacci(4000000, 1, 2, 2)
-   val erg = euler.palindrom000()
-   
-   println(erg)
+    val euler = new Euler();
+
+    //val sum = euler.multiplies3And5(1000,0,0);
+    //val erg = euler.fibonacci(4000000, 1, 2, 2)
+    val erg = euler.palindrom000()
+
+    println(erg)
   }
-  
+
 }
